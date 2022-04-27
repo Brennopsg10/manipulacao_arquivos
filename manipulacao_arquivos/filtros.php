663ce03185fb50095267f0a74c13a69749b62f48
Brenno Phellipe <brennopsg.bp@gmail.com>
2022-04-26 20:20:50 -0400

    criando filtro

--- manipulacao_arquivos/filtros.php
+++ manipulacao_arquivos/filtros.php
@@ -1,7 +1,10 @@
 <?php
 
+require 'MeuFiltro.php';
+
 $arquivo = fopen('lista-cursos.txt', 'r');
 
-stream_filter_append($arquivo, 'string.toupper');
+stream_filter_register('alura.partes', MeuFiltro::class);
+stream_filter_append($arquivo, 'alura.partes');
 
 echo fread($arquivo, filesize('lista-cursos.txt'));
