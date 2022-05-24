7339484c260617fd10116efe688db8a31f7441cd
Brenno Phellipe <brennopsg.bp@gmail.com>
2022-05-24 13:25:09 -0400

    Finalização de manioulação de arquivo CSV com Utf8

--- manipulacao_arquivos/arquivos-para-csv.php
+++ manipulacao_arquivos/arquivos-para-csv.php
@@ -6,12 +6,12 @@ $outrosCursos = file('cursos-php.txt');
 $arquivosCSV = fopen('cursos.csv', 'w');
 
 foreach ($meusCursos as $curso) {
-    $linha = [trim($curso), 'Sim'];
+    $linha = [trim(utf8_decode(($curso))), 'Sim'];
     fputcsv($arquivosCSV, $linha, ';');
 }
 
 foreach ($outrosCursos as $curso) {
-    $linha = [trim($curso), 'Não'];
+    $linha = [trim(utf8_decode(($curso))), utf8_decode('Não')];
     fputcsv($arquivosCSV, $linha, ';');
 }
 fclose($arquivosCSV);
--- manipulacao_arquivos/spl.php
+++ manipulacao_arquivos/spl.php
@@ -4,11 +4,10 @@ $arquivoCuros = new SplFileObject('cursos.csv');
 
 while (!$arquivoCuros->eof()) {
     $linha = $arquivoCuros->fgetcsv(';');
-    echo $linha[0] . PHP_EOL;
+    echo utf8_encode($linha[0]) . PHP_EOL;
 }
 
 $date = new DateTime();
 $date->setTimestamp($arquivoCuros->getCTime());
 
 echo $date->format('d/m/Y');
-
